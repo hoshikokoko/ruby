95440aa645b877396e1186a911e2266fa62477f9
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-10-17 11:21:29 +0000

    [Add]2-8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,12 @@
 def fizz_buzz(number)
-  if number % 3 == 0 && number % 5 == 0
+  if number % 15 == 0
     "FizzBuzz"
   elsif number % 5 == 0
     "Buzz"
   elsif number % 3 == 0
     "Fizz"
   else
-    "#{number}"
+    number.to_s
   end
 end
 
